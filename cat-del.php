5d23cfcdecf59913228362ae3f763443074ceb7e
thanmyatthu <40682189+thanmyatthu@users.noreply.github.com>
2018-06-29 11:40:25 +0530

    Update cat-del.php
    Delete

--- cat-del.php
+++ cat-del.php
@@ -4,5 +4,6 @@ include("confs/config.php");
 $id=$_GET['id'];
 $sql= "DELETE FROM category WHERE id=$id" ;
 mysqli_query($conn,$sql);
+echo"Delete Successful";
 header("location: cat-list.php");
 ?>
